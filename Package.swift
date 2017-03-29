bf891ed4d1d5cb878f9164c77e95cc26f45e25ee
Dmitry Dovgoshliubnyi <darthdimon@googlemail.com>
2017-03-29 15:32:40 +0100

    Initial

---
+++ Package.swift
@@ -0,0 +1,15 @@
+//
+//  Package.swift
+//  Minizip
+//
+//  Created by Dmytro Dovgoshliubnyi on 29/03/2017.
+//  Copyright © 2017 Arthur Dexter. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Minizip",
+    dependencies: [
+    ]
+)
